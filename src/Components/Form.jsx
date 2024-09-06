7fe59981a433031d098035e14cfb77c20f7b3454
aanahia <anahi2654@gmail.com>
2024-09-05 21:15:40 -0500

    Se agregan validaciones al componente Form

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,14 +1,67 @@
 import React from "react";
+import { useState } from "react";
 
 
-
-const Form = () => {
+const Form = ({setSubmittedData}) => {
   //Aqui deberan implementar el form completo con sus validaciones
+  
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+  });
+  const [error, setError] = useState(false);
+
+  const handleChangeName = (event) =>{
+    setFormData({...formData, name: event.target.value});
+  }
+
+  const handleChangeEmail = (event) =>{
+    setFormData({...formData, email: event.target.value});
+  }
+
+  const validateName = (name)=>{
+    return name.trim().length>5 && name==name.trimStart() && !/\d/.test(name);
+  }
+
+  const validateEmail = (email)=>{
+    return  /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  }
+
+  const validateFormData = (formData)=>{
+    return validateName(formData.name) && validateEmail(formData.email);
+  }
+
+  const handleSubmit = (event)=>{
+    event.preventDefault();
+    const isValid = validateFormData(formData);
+    setError(!isValid);
+    
+    if(isValid){
+        setSubmittedData(formData);
+        console.log("Nombre:", formData.name);
+        console.log("Email:", formData.email);
+    }
+    else{
+        setSubmittedData(null);
+    }
+  }
 
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit} >
+        <input type="text" 
+              value={formData.name}
+              placeholder="Full name" 
+              onChange={handleChangeName} />
+        <input type="email" 
+              value={formData.email} 
+              placeholder="Email" 
+              onChange={handleChangeEmail}/>
+        <button>Enviar</button>   
       </form>
+      {
+        error && <span>Por favor verifique su información nuevamente</span>
+      }
     </div>
   );
 };
--- src/Routes/Contact.jsx
+++ src/Routes/Contact.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from 'react';
+import { useState } from "react";
 import Form from '../Components/Form'
 
 //Este componente debera ser estilado como "dark" o "light" dependiendo del theme del Context
 
 const Contact = () => {
+  const [submittedData, setSubmittedData] = useState(null);
+  const [theme, setTheme] = useState('light');
+  
   return (
     <div>
       <h2>Want to know more?</h2>
       <p>Send us your questions and we will contact you</p>
-      <Form/>
+      <Form setSubmittedData={setSubmittedData}/>
+      {
+        submittedData && <span>Gracias {submittedData.name}, te contactaremos cuando antes vía mail</span>
+      }
     </div>
   )
 }
